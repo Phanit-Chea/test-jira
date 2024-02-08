8e50b2eddeb3d73989ecfa65bf348d3eb5cc55a2
Phanit Chea <phanit.chea@student.passerellesnumeriques.org>
2024-02-08 18:36:11 +0700

    [feat] delete-category JC-4

--- models/category.model.php
+++ models/category.model.php
@@ -24,3 +24,10 @@ function getCategory(int $id)
     $statement->execute([':id' => $id]);
     return $statement->fetch();
 }
+function deleteCategory(int $id) : bool
+{
+    global $connection;
+    $statement = $connection->prepare("delete from categories where id = :id");
+    $statement->execute([':id' => $id]);
+    return $statement->rowCount() > 0;
+}
